fdd072f1cba2fe231d3d8f37e4034c206175d632
Demi Sokoya <68524557+demi-xd@users.noreply.github.com>
2021-08-15 12:36:15 -0400

    Added the ContactViewModel.java

---
+++ app/src/main/java/com/example/finalproject/ContactViewModel.java
@@ -0,0 +1,28 @@
+package com.example.finalproject;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ContactViewModel extends AndroidViewModel {
+    private ContactRepository mContactRepository;
+    private LiveData<List<Contact>> mAllContacts;
+
+    public ContactViewModel(Application application){
+        super(application);
+        mContactRepository = new ContactRepository(application);
+        mAllContacts = mContactRepository.getAllContacts();
+    }
+
+    LiveData<List<Contact>> getAllContacts(){
+        return mAllContacts;
+    }
+
+    public void insert(Contact contact){
+        mContactRepository.insert(contact);
+    }
+}
+
